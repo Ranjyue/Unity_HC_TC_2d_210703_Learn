8fd6c1d6b934901f2e89c005782d7b8b633416f0
Anastasia Ranjyue <ranjyue@gmail.com>
2021-07-31 21:20:09 +0800

    Lesson 12
    2D 玩家移動
    2D 玩家跳躍
    
    \ 作業：KID Unity 作業 - C# 方法與 API
    \ 專題：檢查進度
    \ 語法：判斷式 if

--- ROIA_prac_KIDclass/Assets/Scripts/Player.cs
+++ ROIA_prac_KIDclass/Assets/Scripts/Player.cs
@@ -14,9 +14,13 @@ public class Player : MonoBehaviour
 
 
 
-    private Rigidbody2D Rigidbody2D;
+    private Rigidbody2D Rig;
     private AudioSource AudioSource;
     private Animator animator;
+    /// <summary>
+    /// 玩家水平輸入值
+    /// </summary>
+    private float hValue;
     #endregion
 
     #region  事件
@@ -25,13 +29,69 @@ public class Player : MonoBehaviour
     #region 方法
     #endregion
 
-    #region 課堂練習 Lesson 08 Exercise 01: 方法
+    #region  事件
 
     private void Start()
     {
-        herir(8);
-        comerProps("道具名稱");
+        // GetComponent() 泛行方法，可以指定任何類型
+        // 作用：取得此物件的2D剛體元件
+
+        Rig = GetComponent<Rigidbody2D>();
+
     }
+
+    // 一秒約執行60次
+    private void Update()
+    {
+        GetPlayerInputHorizontal();
+        TurnDirection();
+        Jump();
+    }
+
+    //  一秒固定執行 50次，官方建議有使用到物理 API 要在此事件內執行
+    private void FixedUpdate()
+    {
+        Move(hValue);
+    }
+
+    #endregion
+
+
+    #region 方法
+
+    /// <summary>
+    /// 玩家水平輸入值
+    /// </summary>
+    // private float hValue;
+
+    /// <summary>
+    /// 取得玩家輸入水平軸向值：左與右 A、D、左、右
+    /// </summary>
+    /// 
+    private void GetPlayerInputHorizontal()
+    {
+        // TAG：unity Input axis 
+
+        //  水平值=輸入.取得肘向(肘像名稱)
+        //  作用：取得玩家按下水平按鍵的值，按右為 1、按左為 -1、沒按為 0
+        hValue = Input.GetAxis("Horizontal");
+        print("玩家水平值：" + hValue);
+    }
+
+    [Header("重力"), Range(0.1f, 10)]
+    public float gravity = 1;
+
+    #endregion
+
+    #region 課堂練習 Lesson 08 Exercise 01: 方法
+
+    #region practice false
+    //private void Start()
+    //{
+    //herir(8);
+    //comerProps("道具名稱");
+    //}
+    #endregion
 
     // ---------分割線--------- //
 
@@ -77,7 +137,7 @@ public class Player : MonoBehaviour
 
     #endregion
 
-    #region 課堂練習 Lesson 08 Exercise 01:方法 KID
+    #region 課堂練習 Lesson 08 Exercise 01:方法 KID // Lesson 11 接續
 
     /// <summary>
     /// 移動
@@ -85,14 +145,44 @@ public class Player : MonoBehaviour
     /// <param name="horizontal">左右數值</param>
     private void Move(float horizontal)
     {
-        
+        //  區域變數：在方法欄為，有區域性，僅限於此方法內存取
+        //  簡寫：transform 此物件的 Transform 變形元件
+        //  posMOve = 角色當前座標 + 玩家輸入的水平值
+        Vector2 posMove = transform.position + new Vector3(horizontal,-gravity, 0) * speed * Time.fixedDeltaTime;
+        //  鋼體，移動座標(要前往的座標)
+        //  Time.fixedDeltaTime 指 1/50 秒 #讓數值變小
+        Rig.MovePosition(posMove);
+    }
+
+    /// <summary>
+    /// 旋轉方向：處理角色面向問題，按右角度0、按左腳度180
+    /// </summary>
+    private void TurnDirection()
+    {
+        //print("玩家按下右：" + Input.GetKeyDown(KeyCode.D));
+
+        //  如果 玩家按 D 就將角度設定為 0, 0, 0
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+
+        //  如果 玩家按 A 就將角度設定為 0, 180,0
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 180,0);
+        }
     }
     /// <summary>
     /// 跳耀
     /// </summary>
     private void Jump() 
     {
-    
+        //  如果 玩家 按下 空白建 角色就往上跳躍
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Rig.AddForce(new Vector2(0, JumpHeight));
+        }
     }
     /// <summary>
     /// 攻擊
